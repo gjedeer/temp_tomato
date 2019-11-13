cdceedd79456382368648afe43db5b34dfdba4ca
GDR! <gdr@gdr-ThinkPad-X230.gdr.name>
2019-11-13 16:14:58 +0100

    circ buffer with reverse iteration

--- aa.c
+++ aa.c
@@ -26,22 +26,70 @@ char *js_string(char *val) {
 static void webmon_list(char *name, int webmon, int resolve, unsigned int maxcount)
 {
 	FILE *f;
-	char s[512], ip[64], val[256];
 	char *js, *jh;
 	char comma;
 	unsigned long time;
 	unsigned int i;
 	char host[NI_MAXHOST];
+	char ip[64], val[256];
+
+	/* Protection from memory overflow */
+	if (maxcount > 50) {
+	    maxcount = 50;
+	}
 
 	web_printf("\nwm_%s = [", name);
 
 	if (webmon) {
-		/* NASTEPNA LINIA POWINNA ZNOW WYGLADAC TAK: sprintf(s, "/proc/webmon_recent_%s", name); */
-		sprintf(s, "/home/gdr/Downloads/webmon_recent_%s", name);
-		if ((f = fopen(s, "r")) != NULL) {
+		/* NASTEPNA LINIA POWINNA ZNOW WYGLADAC TAK: sprintf(val, "/proc/webmon_recent_%s", name); */
+		sprintf(val, "/home/gdr/Downloads/webmon_recent_%s", name);
+		if ((f = fopen(val, "r")) != NULL) {
 			comma = ' ';
 			i = 0;
-			while ((!maxcount || i++ < maxcount) && fgets(s, sizeof(s), f)) {
+
+			/* Displaying all lines is a special case */
+			if (maxcount == 0) {
+			    char s[512];
+			    while ((!maxcount || i++ < maxcount) && fgets(s, sizeof(s), f)) {
+				    if (sscanf(s, "%lu\t%s\t%s", &time, ip, val) != 3) continue;
+				    jh = NULL;
+				    if (resolve) {
+					    if (resolve_addr(ip, host) == 0)
+						    jh = js_string(host);
+				    }
+				    js = utf8_to_js_string(val);
+				    web_printf("%c['%lu','%s','%s', '%s']", comma,
+					    time, ip, js ? : "", jh ? : "");
+				    free(js);
+				    free(jh);
+				    comma = ',';
+			    }
+			} else {
+			    /* Buffer up to maxcount lines in a circular buffer */
+			    char *buf = malloc(512 * maxcount);
+			    char *s;
+			    char ip[64], val[256];
+			    unsigned int buffer_ptr = 0;
+			    unsigned int total_lines = 0;
+			    unsigned int lines_to_read = 0;
+
+			    while(fgets(buf + 512 * buffer_ptr, 512, f)) {
+				buffer_ptr += 1;
+				total_lines += 1;
+				if(buffer_ptr >= maxcount) {
+				    buffer_ptr = 0;
+				}
+			    }
+			    
+			    /* Now print the remembered lines */
+			    lines_to_read = maxcount;
+			    if(total_lines < lines_to_read) {
+				lines_to_read = total_lines;
+			    }
+			    for (i = lines_to_read; i>0; i--) {
+				const unsigned int index = (buffer_ptr + i - 1) % maxcount;
+				char *s = buf + (index * 512);
+
 				if (sscanf(s, "%lu\t%s\t%s", &time, ip, val) != 3) continue;
 				jh = NULL;
 				if (resolve) {
@@ -54,16 +102,17 @@ static void webmon_list(char *name, int webmon, int resolve, unsigned int maxcou
 				free(js);
 				free(jh);
 				comma = ',';
+
+			    }
 			}
 			fclose(f);
 		}
 	}
 
 	web_puts("];\n");
-	web_printf("wm_%s.reverse()\n", name);
 }
 
 
 int main(int argc, char *argv[]) {
-    webmon_list("domains", 1, 0, 50);
+    webmon_list("domains", 1, 0, 20);
 }
