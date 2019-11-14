5fdcc873042a60af51bec485dd8c66b3bed4acf4
Andrzej Godziuk <andrzej@godziuk.pl>
2019-11-14 18:51:47 +0100

    czw, 14 lis 2019, 18:51:47 CET

--- aa.c
+++ aa.c
@@ -2,13 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <assert.h>
-
 #define web_puts puts
 #define web_printf printf
 #define NI_MAXHOST 512
@@ -29,83 +22,157 @@ char *js_string(char *val) {
     return return_value;
 }
 
+/* Size of each input chunk to be
+   read and allocate for. */
+#ifndef  READALL_CHUNK
+#define  READALL_CHUNK  262144
+#endif
+
+#define  READALL_OK          0  /* Success */
+#define  READALL_INVALID    -1  /* Invalid parameters */
+#define  READALL_ERROR      -2  /* Stream error */
+#define  READALL_TOOMUCH    -3  /* Too much input */
+#define  READALL_NOMEM      -4  /* Out of memory */
+
+/* This function returns one of the READALL_ constants above.
+   If the return value is zero == READALL_OK, then:
+     (*dataptr) points to a dynamically allocated buffer, with
+     (*sizeptr) chars read from the file.
+     The buffer is allocated for one extra char, which is NUL,
+     and automatically appended after the data.
+   Initial values of (*dataptr) and (*sizeptr) are ignored.
+*/
+int readall(FILE *in, char **dataptr, size_t *sizeptr)
+{
+    char  *data = NULL, *temp;
+    size_t size = 0;
+    size_t used = 0;
+    size_t n;
+
+    /* None of the parameters can be NULL. */
+    if (in == NULL || dataptr == NULL || sizeptr == NULL)
+        return READALL_INVALID;
+
+    /* A read error already occurred? */
+    if (ferror(in))
+        return READALL_ERROR;
+
+    while (1) {
+
+        if (used + READALL_CHUNK + 1 > size) {
+            size = used + READALL_CHUNK + 1;
+
+            /* Overflow check. Some ANSI C compilers
+               may optimize this away, though. */
+            if (size <= used) {
+                free(data);
+                return READALL_TOOMUCH;
+            }
+
+            temp = realloc(data, size);
+            if (temp == NULL) {
+                free(data);
+                return READALL_NOMEM;
+            }
+            data = temp;
+        }
+
+        n = fread(data + used, 1, READALL_CHUNK, in);
+        if (n == 0)
+            break;
+
+        used += n;
+    }
+
+    if (ferror(in)) {
+        free(data);
+        return READALL_ERROR;
+    }
+
+    temp = realloc(data, used + 1);
+    if (temp == NULL) {
+        free(data);
+        return READALL_NOMEM;
+    }
+    data = temp;
+    data[used] = '\0';
+
+    *dataptr = data;
+    *sizeptr = used;
+
+    return READALL_OK;
+}
+
 
 static void webmon_list(char *name, int webmon, int resolve, unsigned int maxcount)
 {
 	FILE *f;
 	char *js, *jh;
-	char comma;
+	char comma = ' ';
 	unsigned long time;
 	unsigned int i;
 	char host[NI_MAXHOST];
 	char ip[64], val[256];
 	size_t filesize;
+	char *data;
 
 	web_printf("\nwm_%s = [", name);
 
 	if (webmon) {
-		struct stat st;
-		int fd;
 		/* NASTEPNA LINIA POWINNA ZNOW WYGLADAC TAK: sprintf(val, "/proc/webmon_recent_%s", name); */
-		sprintf(val, "/home/gdr/Downloads/webmon_recent_%s", name);
+		sprintf(val, "/proc/webmon_recent_%s", name);
+//		sprintf(val, "/home/gdr/Downloads/webmon_recent_%s", name);
+//		sprintf(val, "/proc/vmstat");
 
-		if(stat(val, &st) == -1) {
-			perror("Could not stat");
-		}
-		filesize = st.st_size;
+		f = fopen(val, "r");
 
-		fd = open(val, O_RDONLY, 0);
-		if (fd != -1) {
-		    void* mmappedData = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
-		    comma = ' ';
-		    if (mmappedData != MAP_FAILED) {
-			char *data = (char*)mmappedData;
-			char *end = data + filesize;
-                        char *lineStart;
-			int lines_processed = 0;
+		if(f) {
+			int readall_ok;
+			char *data;
 
-                        for(lineStart = end; lineStart >= data; lineStart--) {
-			    if(*lineStart == '\n' || *lineStart == '\r' || lineStart == data) {
-				const int length = end - lineStart;
-                                char *line = malloc(length + 1);
-				char *start = lineStart;
+			readall_ok = readall(f, &data, &filesize);
+			if (readall_ok == READALL_OK) {
+				char *end = data + filesize;
+				char *lineStart;
+				int lines_processed = 0;
 
-				if(start > data) start += 1;
-				strncpy(line, start, length);
-				line[length] = '\0';
+				for(lineStart = end; lineStart >= data; lineStart--) {
+					if(*lineStart == '\n' || *lineStart == '\r' || lineStart == data) {
+						const int length = end - lineStart;
+						char *line = malloc(length + 1);
+						char *start = lineStart;
+
+						if(start > data) start += 1;
+						strncpy(line, start, length);
+						line[length] = '\0';
 
 
-				if (sscanf(line, "%lu\t%s\t%s", &time, ip, val) != 3) continue;
-				jh = NULL;
-				if (resolve) {
-					if (resolve_addr(ip, host) == 0)
-						jh = js_string(host);
+						if (sscanf(line, "%lu\t%s\t%s", &time, ip, val) != 3) continue;
+						jh = NULL;
+						if (resolve) {
+							if (resolve_addr(ip, host) == 0)
+								jh = js_string(host);
+						}
+						js = utf8_to_js_string(val);
+						web_printf("%c['%lu','%s','%s', '%s']", comma,
+								time, ip, js ? : "", jh ? : "");
+						free(js);
+						free(jh);
+						comma = ',';
+
+
+						free(line);
+						end = lineStart;
+
+						lines_processed++;
+
+						if(maxcount > 0 && lines_processed >= maxcount) break;
+					}
 				}
-				js = utf8_to_js_string(val);
-				web_printf("%c['%lu','%s','%s', '%s']", comma,
-					time, ip, js ? : "", jh ? : "");
-				free(js);
-				free(jh);
-				comma = ',';
-
-
-				free(line);
-				end = lineStart;
-
-				lines_processed++;
-
-				if(maxcount > 0 && lines_processed >= maxcount) break;
-			    }
+			} else {
+				printf("readall error %d", readall_ok);
 			}
-
-			munmap(mmappedData, filesize);
-		    } else {
-			perror("Could not mmap");
-			printf("Tried to mmap file %s, %ld bytes", val, filesize);
-		    }
-		    close(fd);
-		} else {
-		    perror("Could not open file");
+			fclose(f);
 		}
 	}
 
